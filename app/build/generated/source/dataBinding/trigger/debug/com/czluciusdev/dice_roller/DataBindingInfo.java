740f76949731965b11850bc32caba9b0c5902a72
czlucius <58442255+czlucius@users.noreply.github.com>
2021-07-03 17:01:52 +0800

    Remove build files

--- app/build/generated/source/dataBinding/trigger/debug/com/czluciusdev/dice_roller/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.czluciusdev.dice_roller;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
--- app/build/generated/source/kapt/debug/com/czluciusdev/diceroller/BR.java
+++
@@ -1,5 +0,0 @@
-package com.czluciusdev.diceroller;
-
-public class BR {
-  public static final int _all = 0;
-}
